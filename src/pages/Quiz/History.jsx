db8ade6ec2a1213fde7cd6dfafcb991ef0474c7d
Trong An <troandev@gmail.com>
2024-06-16 23:57:22 +0700

    new history

--- src/pages/Quiz/History.jsx
+++ src/pages/Quiz/History.jsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { get, post } from "../../utils/request";
+import { get, get_firebase, post } from "../../utils/request";
 import { getCookie } from "../../helpers/cookie";
 import Swal from "sweetalert2";
 import { collection, getDocs, getFirestore } from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import sortArrayByTime from "../../helpers/sort";
 export default function History() {
-    const [quiz, setQuiz] = useState([]);
-
     const db = getFirestore();
 
     const [user, setUser] = useState({});
+    const [history, setHistory] = useState([]);
+
     const auth = getAuth();
 
     useEffect(() => {
@@ -22,36 +23,45 @@ export default function History() {
             });
         };
         handleCheckLogin();
-    }, []);
+    }, [auth]);
     console.log(user.uid);
 
     useEffect(() => {
-        const fetchBook = async () => {
-            const querySnapshot = await getDocs(collection(db, "histories"));
-            querySnapshot.forEach((doc) => {
-                setQuiz((prev) => [
-                    ...prev,
-                    {
-                        id: doc.id,
-                        data: doc.data(),
-                    },
-                ]);
-            });
-        };
-        fetchBook();
-    }, []);
+        if (user) {
+            const fetchQuiz = async () => {
+                const querySnapshot = await getDocs(collection(db, "histories"));
+                const filteredQuiz = [];
+
+                querySnapshot.forEach((doc) => {
+                    const data = doc.data();
+                    console.log(data);
+                    if (data.uid === user.uid) {
+                        filteredQuiz.push({
+                            ...doc.data(),
+                            id: doc.id,
+                        });
+                    }
+                });
+
+                const result = sortArrayByTime(filteredQuiz);
+
+                setHistory(result);
+            };
+
+            fetchQuiz();
+        }
+    }, [user, db]);
 
     // console.log(quiz)
-    console.log(quiz);
+    console.log(history);
     return (
         <div>
             <p className="text-2xl font-bold text-green-500">Lịch sử làm bài</p>
-            {/* <div className="grid grid-cols-4 gap-5 my-5">
+            <div className="grid grid-cols-4 gap-5 my-5">
+                {history.length === 0 ? "Bạn chưa có lịch sử làm bài..." : ""}
                 {history.map((item) => (
                     <div key={item.id} className="bg-white p-3 border-[1px] shadow-md">
-                        <h1 className="text-lg mb-3">
-                            Tên bài thi: <label className="text-green-500 font-bold">{item.title}</label>{" "}
-                        </h1>
+                        <h1 className="text-lg mb-3 text-green-500 font-bold h-[56px] line-clamp-2">{item.title}</h1>
                         <p>
                             Số câu đúng:{" "}
                             <label className="text-green-500 font-bold">
@@ -59,12 +69,12 @@ export default function History() {
                             </label>{" "}
                         </p>
                         <p className="text-gray-500">Ngày thi: {item.date}</p>
-                        <a href={`answer/${item.topic_id}`} className="block text-right mt-3">
+                        <a href={`answer/${item.id}`} className="block text-right mt-3">
                             <button className="bg-green-500 text-white ">Xem chi tiết</button>
                         </a>
                     </div>
                 ))}
-            </div> */}
+            </div>
         </div>
     );
 }
