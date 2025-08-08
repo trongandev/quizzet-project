5c0342eb414b821e7e13c0e40efc1ef8e3c27619
Trong An <troandev@gmail.com>
2025-08-08 21:18:38 +0700

    fix: update quiz state type for improved type safety in QuizDetail component

--- src/app/(app)/quiz/detail/[slug]/page.tsx
+++ src/app/(app)/quiz/detail/[slug]/page.tsx
@@ -3,12 +3,12 @@ import { GET_API_WITHOUT_COOKIE } from "@/lib/fetchAPI"
 import React, { Suspense, useEffect, useState } from "react"
 
 import { useUser } from "@/context/userContext"
-import { IComment, IQuestion, IQuiz } from "@/types/type"
+import { IComment, IDataQuiz, IQuiz } from "@/types/type"
 import DetailQuiz from "@/components/quiz/DetailQuiz"
 import { LoadingScreen } from "@/components/LoadingScreen"
 export default function QuizDetail({ params }: { params: any }) {
     const [data, setData] = useState<IQuiz>()
-    const [quiz, setQuiz] = useState<IQuestion[]>()
+    const [quiz, setQuiz] = useState<IDataQuiz[]>()
     const [comment, setComment] = useState<IComment[]>([])
     const userContext = useUser()
     const user = userContext?.user
